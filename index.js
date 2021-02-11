cfdcb3be4382137e385cfbd96ee22e4d982d0f68
Jamie OBeirne <jamieobeirne123@gmail.com>
2021-02-11 13:51:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+/*by Jamie O'Beirne
+jamieobeirne123@gmail.com</p>
+11 February 2021</p>*/
+
+
+1
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.getElementById("column").className = "toggleListView";
+        } else {
+            document.getElementById("column").className = "column";
+        }
+    });
+});
+
+
+2
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.getElementById("column2").className = "toggleListView";
+        } else {
+            document.getElementById("column2").className = "column";
+        }
+    });
+});
+
+
+3
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.getElementById("column3").className = "toggleListView";
+        } else {
+            document.getElementById("column3").className = "column";
+        }
+    });
+});
+
+
+4
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.getElementById("column4").className = "toggleListView";
+        } else {
+            document.getElementById("column4").className = "column";
+        }
+    });
+});
+
+
+5
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            document.getElementById("column5").className = "toggleListView";
+        } else {
+            document.getElementById("column5").className = "column";
+        }
+    });
+});
+
+
+/*by Jamie O'Beirne
+jamieobeirne123@gmail.com</p>
+11 February 2021</p>*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
